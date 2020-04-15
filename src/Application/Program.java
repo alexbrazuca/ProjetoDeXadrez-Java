f82cbf557d14dfbdb178351791200c3918151af1
Alex Silva <alexbrazuca@live.com>
2020-04-15 16:16:47 -0300

    Começando a implementar tabuleiro e peças

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,19 +2,14 @@ package Application;
 
 import java.util.Scanner;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-		
-		System.out.println("informe a linha: ");
-		int linha = sc.nextInt();
-		System.out.println("informe a coluna: ");
-		int coluna = sc.nextInt();
-		Position posicao = new Position(linha,coluna);
-		System.out.println(posicao);
+		Board board = new Board(10,10);
+		System.out.println(board);
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,31 +1,32 @@
 package boardgame;
 
-public class Position {
-	
+public class Board {
+
 	private int rows;
 	private int columns;
+	private Piece[][] pieces;
 	
-	public Position(int rows, int colmuns) {
-		this.rows =rows;
-		this.columns = colmuns;
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
 	}
 
-	
 	public int getRows() {
 		return rows;
 	}
+
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
+
 	public int getColumns() {
 		return columns;
 	}
+
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-	@Override
-	public String toString() {
-		return rows + ", " + columns; 
-	}
-
+	
+	
+	
 }
