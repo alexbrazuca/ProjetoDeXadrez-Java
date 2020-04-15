c0ee621a3bd34eeb8c88f53735bd53481d0d21a2
Alex Silva <alexbrazuca@live.com>
2020-04-15 17:07:24 -0300

    Camada Xadrez e imprimindo o tabuleiro

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,15 +1,17 @@
 package Application;
 
-import java.util.Scanner;
+import Chess.ChessMatch;
 
-import boardgame.Board;
-import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(10,10);
-		System.out.println(board);
+		
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+	
+	
 	}
 
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,30 +2,30 @@ package boardgame;
 
 public class Position {
 	
-	private int rows;
-	private int columns;
+	private int row;
+	private int column;
 	
-	public Position(int rows, int colmuns) {
-		this.rows =rows;
-		this.columns = colmuns;
+	public Position(int row, int colmu) {
+		this.row =row;
+		this.column = column;
 	}
 
 	
-	public int getRows() {
-		return rows;
+	public int getRow() {
+		return row;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
+	public void setRow(int row) {
+		this.row = row;
 	}
-	public int getColumns() {
-		return columns;
+	public int getColumn() {
+		return column;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
+	public void setColumns(int column) {
+		this.column = column;
 	}
 	@Override
 	public String toString() {
-		return rows + ", " + columns; 
+		return row + ", " + column; 
 	}
 
 }
