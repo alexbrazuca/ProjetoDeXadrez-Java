24b2c4346540383bf8e9d0015e572900ce19e08a
Alex Silva <alexbrazuca@live.com>
2020-04-17 13:33:07 -0300

    Logica de checkmate

---
+++ bin/chess/pieces/Bishop.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	public String toString() {
+		return "B";
+	}
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+}
