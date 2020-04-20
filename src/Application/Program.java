28e87fe17d55ab04dd5fb89a558a4701b07030de
Alex Silva <alexbrazuca@live.com>
2020-04-19 22:59:48 -0300

    Promoção dos peões

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -39,6 +39,11 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if(chessMatch.getPromoted()!= null) {
+					System.out.print("Enter piece for promotion (B/R/N/Q): ");
+					String type  = sc.nextLine();
+					chessMatch.replacedPromotedPiece(type);
+				}
 			}
 			catch(ChessException e){
 				System.out.println(e.getMessage());
